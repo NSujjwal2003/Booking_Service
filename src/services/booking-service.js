e77ba78dc8aa46f9dd1cfdac1cbcb6d5b6873bfc
NSujjwal2003 <ujjwal.22gcebcs042@galgotiacollege.edu>
2025-05-05 12:30:00 +0530

    implemented first booking transaction

---
+++ src/services/booking-service.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+
+const {BookingRepository} = require('../repositories');
+const { ServerConfig } = require('../config');
+
+const db = require('../models');
+
+async function createBooking(data){
+    try{
+        const result = db.sequelize.transaction(async function bookingImpl(t){
+            const flight = await axios.get(`${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${data.flightId}`);
+            console.log(flight);
+            return true;
+        });
+    } catch(error){
+        console.log(error);
+        throw error;
+    }
+}
+
+module.exports = {
+    createBooking
+};

e6c4fb79683275083db23d00d090b5f0a939a31e
NSujjwal2003 <ujjwal.22gcebcs042@galgotiacollege.edu>
2025-06-09 19:52:57 +0530

    message queue implemented using rabbitMq

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 const express = require('express');
+const amqplib = require('amqplib'); 
+
+async function connectQueue(){
+    try {
+        const connection = await amqplib.connect('amqp://localhost');
+        const channel = await connection.createChannel();
+        await channel.assertQueue("noti-queue");
+        // setInterval(() => {
+        //     channel.sendToQueue("noti-queue", Buffer.from('smth to do'));
+        // }, 1000);
+    } catch (error) {
+        console.log(error);
+    }    
+}
 
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
@@ -15,4 +29,5 @@ app.use('/bookingService', apiRoutes);
 app.listen(ServerConfig.PORT, () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
     CRON();
+    connectQueue();
 });
